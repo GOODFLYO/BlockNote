dce9d0017500a31484f936f3b1f2124324609229
goodfly <2084813108@qq.com>
2022-12-21 00:00:48 +0800

    04

--- 0CreateMd.bat
+++ 0CreateMd.bat
@@ -1,3 +1,17 @@
 @echo off
-echo ^<h1 style=" font-size:32px;text-align:center;color:red"^>^</h1^> ^[toc^] > 202_zyn_xx.md
+
+echo --- > 202_zyn_xx.md
+echo title: 区块链学习第x期 >> 202_zyn_xx.md
+echo date: 2022-11-01 18:00:00 >> 202_zyn_xx.md
+echo tags: >> 202_zyn_xx.md
+echo     - >> 202_zyn_xx.md
+echo categories: >> 202_zyn_xx.md
+echo     - 区块链 >> 202_zyn_xx.md
+echo --- >> 202_zyn_xx.md
+echo [myBlog](https://goodflyo.github.io/) >> 202_zyn_xx.md
+echo ^<h1 style=" font-size:32px;text-align:center;color:red"^>^</h1^> >> 202_zyn_xx.md
+echo [toc] >> 202_zyn_xx.md
+echo "<!-- more -->" >> 202_zyn_xx.md
+echo --- >> 202_zyn_xx.md
+
 echo "202_zyn_xx.md file created successfully."
--- 202_zyn_xx.md
+++
@@ -1,1 +0,0 @@
-<h1 style=" font-size:32px;text-align:center;color:red"></h1> [toc] 
