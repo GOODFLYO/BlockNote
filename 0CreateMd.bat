288d94eea87aefbb845fe314c362045f4deaf21d
goodfly <2084813108@qq.com>
2022-12-20 23:10:35 +0800

    04

---
+++ 0CreateMd.bat
@@ -0,0 +1,3 @@
+@echo off
+echo ^<h1 style=" font-size:32px;text-align:center;color:red"^>^</h1^> ^[toc^] > 202_zyn_xx.md
+echo "202_zyn_xx.md file created successfully."
---
+++ 202_zyn_xx.md
@@ -0,0 +1,1 @@
+<h1 style=" font-size:32px;text-align:center;color:red"></h1> [toc] 
